2a1594ae1babc9b98ed3a6191d06278e2b3dd016
krsh <kkouroshm@yahoo.com>
2023-06-05 00:46:23 -0500

    cyruspart9

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -20,12 +20,12 @@ function UserSettings() {
   const modal__dimRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
   const user = getAuth().currentUser;
-  userIsPremium = usePremiumStatus(user);
-  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true)
+  const userIsPremium = usePremiumStatus(user);
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(true);
 
   const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen)
-  }
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   const premiumStatus = usePremiumStatus(user);
 
   useEffect(() => {
@@ -55,9 +55,7 @@ function UserSettings() {
 
   return (
     <div>
-
-      <div className={`sidebar ${isModalOpen ? "dimmed" : ""}`}></div>
-      <SidebarSizing isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <div className={` ${isModalOpen ? "dimmed" : ""}`}></div>
       {isLoading ? (
         <section>
           <div className="container">
@@ -88,73 +86,79 @@ function UserSettings() {
           </div>
         </section>
       ) : (
-       <SidebarSizing isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <section>
-          <div className="container">
-            <div className="row setting__row">
-              <div className="section__title page__title">Settings</div>
-              {email ? (
-                <>
-                  {premiumStatus ? (
-                    <div className="setting__content">
-                      <div className="settings__sub--title">
-                        Your Subscription plan
+        <>
+          <SidebarSizing
+            isSidebarOpen={isSidebarOpen}
+            toggleSidebar={toggleSidebar}
+          />
+
+          <section>
+            <div className="container">
+              <div className="row setting__row">
+                <div className="section__title page__title">Settings</div>
+                {email ? (
+                  <>
+                    {premiumStatus ? (
+                      <div className="setting__content">
+                        <div className="settings__sub--title">
+                          Your Subscription plan
+                        </div>
+                        <div className="settings__text">Premium</div>
                       </div>
-                      <div className="settings__text">Premium</div>
-                    </div>
-                  ) : (
-                    <div className="setting__content">
-                      <div className="settings__sub--title">
-                        Your Subscription plan
+                    ) : (
+                      <div className="setting__content">
+                        <div className="settings__sub--title">
+                          Your Subscription plan
+                        </div>
+                        <div className="settings__text">Basic</div>
+                        <button
+                          className="btn settings__upgrade--btn"
+                          onClick={() => router.push("/choose-plan")}
+                        >
+                          Upgrade to Premium
+                        </button>
                       </div>
-                      <div className="settings__text">Basic</div>
-                      <button
-                        className="btn settings__upgrade--btn"
-                        onClick={() => router.push("/choose-plan")}
-                      >
-                        Upgrade to Premium
-                      </button>
+                    )}
+                    <div className="setting__content">
+                      <div className="settings__sub--title">Email</div>
+                      <div className="settings__text">{email}</div>
                     </div>
-                  )}
-                  <div className="setting__content">
-                    <div className="settings__sub--title">Email</div>
-                    <div className="settings__text">{email}</div>
+                  </>
+                ) : (
+                  <div className="settings__login--wrapper">
+                    <img
+                      alt="login"
+                      src="https://summarist.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogin.e313e580.png&w=1080&q=75"
+                      decoding="async"
+                      data-nimg="1"
+                      loading="lazy"
+                      width="1033"
+                      height="712"
+                    />
+                    <div className="settings__login--text">
+                      Log in to your account to see your details.
+                    </div>
+                    <button
+                      className="btn settings__login--btn"
+                      onClick={openModal}
+                    >
+                      Login
+                    </button>
                   </div>
-                </>
-              ) : (
-                <div className="settings__login--wrapper">
-                  <img
-                    alt="login"
-                    src="https://summarist.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogin.e313e580.png&w=1080&q=75"
-                    decoding="async"
-                    data-nimg="1"
-                    loading="lazy"
-                    width="1033"
-                    height="712"
-                  />
-                  <div className="settings__login--text">
-                    Log in to your account to see your details.
-                  </div>
-                  <button
-                    className="btn settings__login--btn"
-                    onClick={openModal}
-                  >
-                    Login
-                  </button>
-                </div>
-              )}
+                )}
+              </div>
             </div>
-          </div>
-          {isModalOpen && (
-            <div
-              className="modal__dim"
-              ref={modal__dimRef}
-              onClick={handleOverlayClick}
-            >
-              <LogInModal openModal={openModal} />
-            </div>
-          )}
-        </section>
+            {isModalOpen && (
+              <div
+                className="modal__dim"
+                ref={modal__dimRef}
+                onClick={handleOverlayClick}
+              >
+                <LogInModal openModal={openModal} />
+              </div>
+            )}
+          </section>
+        </>
       )}
     </div>
   );
